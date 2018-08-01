f759f0fe4002ea75687ce2574ee46339c7f583e9
jnickson1506@gmail.com <ec2-user@ip-172-31-17-43.us-east-2.compute.internal>
2018-08-01 16:43:03 +0000

    Exercise 7

--- directory.rb
+++ directory.rb
@@ -13,15 +13,26 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students".center(50)
 end
 
+def check_if_month(month)
+  return ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december', ''].include? month.downcase
+end
+
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the names of the students followed by the cohort"
   puts "To finish, just fit return twice"
   students = []
-  name = gets.chomp
-  while !name.empty? do
-    students << {name: name, cohort: :august}
-    puts "Now we have #{students.count} students"
+  name = ' '
+  loop do
+    cohort = ' '
     name = gets.chomp
+    break if name.empty?
+      until check_if_month(cohort)
+        cohort = gets.chomp
+        puts "Please enter the cohort correctly" if !check_if_month(cohort)
+      end
+    cohort = "august" if cohort.empty?
+    students << {name: name, cohort: cohort}
+    puts "Now we have #{students.count} students"
   end
   students
 end
