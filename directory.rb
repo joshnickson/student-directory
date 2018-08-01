3a0abf9fe6fac86134c75c40f29ee2e9a9b593f1
jnickson1506@gmail.com <ec2-user@ip-172-31-17-43.us-east-2.compute.internal>
2018-08-01 12:53:35 +0000

    Exercise 3

--- directory.rb
+++ directory.rb
@@ -4,7 +4,7 @@ def print_header
 end
 def print(students)
   students.each_with_index do |student, index| 
-    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].chars.first == 'A'
+    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length < 12
   end
 end
 def print_footer(names)
